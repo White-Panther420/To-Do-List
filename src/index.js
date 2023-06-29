1f933e87af005b0cdfd8481bfc754d7baeaf0d86
White_Panther <99096313+White-Panther420@users.noreply.github.com>
2023-06-28 17:06:24 -0700

    Worked on GUI
    Most of the basic GUI is finished, I now need to get the OOP side of things done for the to-do list and projects so I can create their GUIs. Currently working on pop up form for new task.

--- src/index.js
+++ src/index.js
@@ -1,122 +1,91 @@
 import "./Styles/style.css"
-import Logo from "./Assets/Logo.png"
-import HomeIcon from "./Assets/Home.svg"
-import TodayIcon from "./Assets/Today.svg"
-import WeekIcon from "./Assets/Week.svg"
-import ImportantIcon from "./Assets/Important.svg"
-import CompleteIcon from "./Assets/Complete.svg"
-import ProjectIcon from "./Assets/Project.png"
-import EditIcon from "./Assets/Edit.png"
-import DeleteIcon from "./Assets/Delete.png"
+import { loadPage, createAnElement, createLabel, createInput } from "./loadPage"
+/******************** GUI FUNCTIONS *********************/
+const createForm = ()=>{
+    const popUpForm = createAnElement("div", "popUpForm")
+    const formTitleDiv = createAnElement("div", "formTitleDiv")
+    const formTItle = createAnElement("h2", "formTItle")
+    formTItle.textContent = "Add Task"
+    const exitBtn = createAnElement("button", "exitBtn")
+    exitBtn.textContent = "x"
+    formTitleDiv.appendChild(exitBtn)
+    formTitleDiv.appendChild(formTItle)
+
+    const formBody = createAnElement("div", "formBody")
+    const form = createAnElement("form", "form")
+    formBody.appendChild(form)
+    
+    const taskTitleDiv = createAnElement("div", "formField")
+    form.appendChild(taskTitleDiv)
+    const titleLabel = createLabel("Title")
+    const titleInput = createInput("Title")
+    taskTitleDiv.appendChild(titleLabel)
+    taskTitleDiv.appendChild(titleInput)
+    //popUpForm.appendChild(formTitleDiv)
+
+}
+/******************** GUI FUNCTIONS *********************/
+
+class Task{
+    constructor(title, description, notes="", dueDate, priority){
+        this.title = title
+        this.description = description
+        this.notes = notes
+        this.dueDate = dueDate
+        this.priority = priority
+    }
+
+    //Getters
+    getDueDate(){
+        return this.dueDate
+    }
+    getPriority(){
+        return this.priority
+    }
+
+    //Setters
+    setTitle(title){
+        return this.title = title
+    }
+    setDescription(description){
+        return this.description = description
+    }
+    setNotes(notes){
+        return this.notes = notes
+    }
+    setDueDate(dueDate){
+        return this.dueDate = dueDate
+    }
+    setPriority(priority){
+        return this.priority = priority
+    }
+    
+}
+
+class ToDoList{
+    constructor(){
+        this.list = []
+    }
+}
 
 const content = document.querySelector("#content")
+content.appendChild(loadPage())
 
-const createSideBarOption = (importedIconName, optionName) =>{
-    const optionDiv = createAnElement("div", "optionDiv")
-    optionDiv.setAttribute("id", "optionDiv")
-    const optionIcon = createAnImg(importedIconName, "optionIcon")
-    optionDiv.appendChild(optionIcon)
-    const optionText = createAnElement("p", "optionText")
-    optionText.textContent = optionName
-    optionDiv.appendChild(optionText)
-    return optionDiv
-}
-const createProject = (projectName) =>{
-    const projectDiv = createAnElement("div", "projectDiv")
-    const leftSideDiv = createAnElement("div", "leftSideDiv")
-    const projectIcon = createAnImg(ProjectIcon, "projectIconLeft")
-    const projectNameP = createAnElement("p", "projectNameP")
-    projectNameP.textContent = projectName
-    leftSideDiv.appendChild(projectIcon)
-    leftSideDiv.appendChild(projectNameP)
-    
-    const rightSideDiv = createAnElement("div", "rightSideDiv")
-    const editIcon = createAnImg(EditIcon, "projectIcon")
-    const deleteIcon = createAnImg(DeleteIcon, "projectIcon")
-    rightSideDiv.appendChild(editIcon)
-    rightSideDiv.appendChild(deleteIcon)
-
-    projectDiv.appendChild(leftSideDiv)
-    projectDiv.appendChild(rightSideDiv)
-    return projectDiv
-}
-const createAnElement = (elementName, className) =>{
-    const element = document.createElement(elementName)
-    element.classList.add(className)
-    return element
-}
-const createAnImg = (importedImage, className) =>{
-    const myImg = new Image()
-    myImg.src = importedImage
-    myImg.classList.add(className)
-    return myImg
-}
-
-//HEADER
-const headerDiv = createAnElement("div", "headerDiv")
-const pageTitle = createAnElement("h1", "pageTitle")
-const siteLogo = createAnImg(Logo, "siteLogo")
-pageTitle.textContent = "To Do List"
-headerDiv.appendChild(siteLogo)
-headerDiv.appendChild(pageTitle)
-
-//SIDEBAR
-const sideBarDiv = createAnElement("div", "sideBarDiv")
-const mainOptionsDiv = createAnElement("div", "mainOptionsDiv")
-const icons = [HomeIcon, TodayIcon, WeekIcon, ImportantIcon, CompleteIcon]
-const optionNames = ["Home", "Today", "Week", "Important", "Completed"]
-for(let i=0; i<optionNames.length; i++){
-    mainOptionsDiv.appendChild(createSideBarOption(icons[i], optionNames[i]))
-}
-sideBarDiv.appendChild(mainOptionsDiv)
-
-const projectOptionDiv = createAnElement("div", "projectOptionDiv")
-const projectHeaderDiv = createAnElement("div", "projectHeaderDiv")
-const projectHeader = createAnElement("h2", "projectHeader")
-projectHeader.textContent = "Projects "
-projectHeaderDiv.appendChild(projectHeader)
-const numProjeccts = createAnElement("h2", "numProjeccts")
-numProjeccts.textContent = " (0)"
-projectHeader.appendChild(numProjeccts)
-const addProjectBtn = createAnElement("button", "addProjectBtn")
-projectHeaderDiv.appendChild(addProjectBtn)
-const projectsContainer = createAnElement("div", "projectsContainer")
-projectOptionDiv.appendChild(projectHeaderDiv)
-projectOptionDiv.appendChild(projectsContainer)
-const projectNames = ["Psssssssroject1", "Project2", "Project3"]
-for(let i=0; i<projectNames.length; i++){
-    projectOptionDiv.appendChild(createProject(projectNames[i]))
-}
-
-sideBarDiv.appendChild(projectOptionDiv)
-
-const footer = createAnElement("footer", "footer")
-const gitHubUsername = createAnElement("a", "gitHubUsername")
-const credit = createAnElement("p", "credit")
-credit.textContent = "Created by "
-gitHubUsername.textContent = "White-Panther420"
-gitHubUsername.href = "https://github.com/White-Panther420"
-credit.appendChild(gitHubUsername)
-footer.appendChild(credit)
-
-sideBarDiv.appendChild(footer)
-
-//TO-DO LIST
-const toDoMainContent = createAnElement("div", "toDoMainContent")
-const tabTitle = createAnElement("h2", "tabTitle")
-const taksAndToDoDIv = createAnElement('div', "taksAndToDoDIv")
-const taskDiv = createAnElement('div', "taskDiv")
-const taskP = createAnElement("p", "taskP")
-taskP.textContentP = "Tasks"
-const numTasks = createAnElement("p", "numTasks")
-numTasks.textContent = "(0)"
 
 const toDoListDiv = createAnElement("div", "toDoListDiv")
+const newList = new ToDoList()  //New array to store task objects
+
+const addTaskbtn = document.querySelector(".addTaskBtn")
+addTaskbtn.addEventListener("click", ()=>{
+    newList.push(createNewTask())
+})
+
+const createNewTask = ()=>{
+
+}
 
 
 
-toDoMainContent.appendChild(tabTitle)
-toDoMainContent.appendChild(taksAndToDoDIv)
 
 
 
@@ -127,9 +96,26 @@ toDoMainContent.appendChild(taksAndToDoDIv)
 
 
 
-content.appendChild(headerDiv)
-content.appendChild(sideBarDiv)
-content.appendChild(toDoMainContent)
 
 
 
+
+
+
+
+
+
+
+
+
+
+const mainSidebarOptions = document.querySelectorAll(".optionDiv")
+mainSidebarOptions.forEach(option => {
+    option.addEventListener("click", ()=>{
+        // let optionIcon = option.getElementByTagName(img)
+        // tabIcon.src = optionIcon.src
+        // console.log(option.src)
+        const tabTitle = document.querySelector(".tabTitle")
+        tabTitle.textContent = option.textContent
+    })
+});
---
+++ src/loadPage.js
@@ -0,0 +1,155 @@
+import "./Styles/style.css"
+import Logo from "./Assets/Logo.png"
+import HomeIcon from "./Assets/Home.svg"
+import TodayIcon from "./Assets/Today.svg"
+import WeekIcon from "./Assets/Week.svg"
+import ImportantIcon from "./Assets/Important.svg"
+import CompleteIcon from "./Assets/Complete.svg"
+import ProjectIcon from "./Assets/Project.png"
+import EditIcon from "./Assets/Edit.png"
+import DeleteIcon from "./Assets/Delete.png"
+/******************** HELPER FUNCTIONS *********************/
+const createSideBarOption = (importedIconName, optionName) =>{
+    const optionDiv = createAnElement("div", "optionDiv")
+    const optionIcon = createAnImg(importedIconName, "optionIcon")
+    optionDiv.appendChild(optionIcon)
+    const optionText = createAnElement("p", "optionText")
+    optionText.textContent = optionName
+    optionDiv.appendChild(optionText)
+    return optionDiv
+}
+const createProject = (projectName) =>{
+    const projectDiv = createAnElement("div", "projectDiv")
+    const leftSideDiv = createAnElement("div", "leftSideDiv")
+    const projectIcon = createAnImg(ProjectIcon, "projectIconLeft")
+    const projectNameP = createAnElement("p", "projectNameP")
+    projectNameP.textContent = projectName
+    leftSideDiv.appendChild(projectIcon)
+    leftSideDiv.appendChild(projectNameP)
+    
+    const rightSideDiv = createAnElement("div", "rightSideDiv")
+    const editIcon = createAnImg(EditIcon, "projectIcon")
+    const deleteIcon = createAnImg(DeleteIcon, "projectIcon")
+    rightSideDiv.appendChild(editIcon)
+    rightSideDiv.appendChild(deleteIcon)
+
+    projectDiv.appendChild(leftSideDiv)
+    projectDiv.appendChild(rightSideDiv)
+    return projectDiv
+}
+const createAnElement = (elementName, className) =>{
+    const element = document.createElement(elementName)
+    element.classList.add(className)
+    return element
+}
+const createAnImg = (importedImage, className) =>{
+    const myImg = new Image()
+    myImg.src = importedImage
+    myImg.classList.add(className)
+    return myImg
+}
+const createLabel = (forAttributeName) =>{
+    const label = createAnElement("label", "label")
+    label.setAttribute("for", forAttributeName.toLowerCase())
+    label.textContent = forAttributeName
+    return label
+}
+const createInput = (nameAttribute)=>{
+    const input = createAnElement("input", "input")
+    input.setAttribute("type", "text")
+    input.setAttribute("name", nameAttribute.toLowerCase())
+    input.setAttribute("id", nameAttribute.toLowerCase())
+    return input
+}
+/******************** ELEMENT CREATION HELPER FUNCTIONS *********************/
+
+const loadPage = () =>{
+    //HEADER
+    const pageContent = createAnElement("div", "pageContent")
+    const headerDiv = createAnElement("div", "headerDiv")
+    const pageTitle = createAnElement("h1", "pageTitle")
+    const siteLogo = createAnImg(Logo, "siteLogo")
+    pageTitle.textContent = "To Do List"
+    headerDiv.appendChild(siteLogo)
+    headerDiv.appendChild(pageTitle)
+
+    //SIDEBAR
+    const sideBarDiv = createAnElement("div", "sideBarDiv")
+    const mainOptionsDiv = createAnElement("div", "mainOptionsDiv")
+    const icons = [HomeIcon, TodayIcon, WeekIcon, ImportantIcon, CompleteIcon]
+    const optionNames = ["Home", "Today", "Week", "Important", "Completed"]
+    for(let i=0; i<optionNames.length; i++){
+        mainOptionsDiv.appendChild(createSideBarOption(icons[i], optionNames[i]))
+    }
+    sideBarDiv.appendChild(mainOptionsDiv)
+
+    const projectOptionDiv = createAnElement("div", "projectOptionDiv")
+    const projectHeaderDiv = createAnElement("div", "projectHeaderDiv")
+    const projectHeader = createAnElement("h2", "projectHeader")
+    projectHeader.textContent = "Projects "
+    projectHeaderDiv.appendChild(projectHeader)
+    const numProjeccts = createAnElement("h2", "numProjeccts")
+    numProjeccts.textContent = " (0)"
+    projectHeader.appendChild(numProjeccts)
+    const addProjectBtn = createAnElement("button", "addProjectBtn")
+    projectHeaderDiv.appendChild(addProjectBtn)
+    const projectsContainer = createAnElement("div", "projectsContainer")
+    projectOptionDiv.appendChild(projectHeaderDiv)
+    projectOptionDiv.appendChild(projectsContainer)
+    const projectNames = ["Psssssssroject1", "Project2", "Project3"]
+    for(let i=0; i<projectNames.length; i++){
+        projectOptionDiv.appendChild(createProject(projectNames[i]))
+    }
+
+    sideBarDiv.appendChild(projectOptionDiv)
+
+    const footer = createAnElement("footer", "footer")
+    const gitHubUsername = createAnElement("a", "gitHubUsername")
+    const credit = createAnElement("p", "credit")
+    credit.textContent = "Created by "
+    gitHubUsername.textContent = "White-Panther420"
+    gitHubUsername.href = "https://github.com/White-Panther420"
+    credit.appendChild(gitHubUsername)
+    footer.appendChild(credit)
+
+    sideBarDiv.appendChild(footer)
+
+    //TO-DO LIST
+    const toDoMainContent = createAnElement("div", "toDoMainContent")
+    const tabTitleDiv = createAnElement("div", "tabTitleDiv")
+    const tabIcon = createAnImg(HomeIcon, "tabIcon")
+    const tabTitle = createAnElement("h2", "tabTitle")
+    tabTitle.textContent = "Home"
+    tabTitleDiv.appendChild(tabIcon)
+    tabTitleDiv.appendChild(tabTitle)
+
+    const taksAndToDoDIv = createAnElement('div', "taksAndToDoDIv")
+    const taskDiv = createAnElement('div', "taskDiv")
+    taksAndToDoDIv.appendChild(taskDiv)
+    const taskP = createAnElement("p", "taskP")
+    taskP.textContent = "Tasks"
+    const numTasks = createAnElement("p", "numTasks")
+    numTasks.textContent = "(0)"
+    const addTaskBtn = createAnElement("button", "addTaskBtn")
+    taskDiv.appendChild(taskP)
+    taskDiv.appendChild(numTasks)
+    taskDiv.appendChild(addTaskBtn)
+    
+
+    toDoMainContent.appendChild(tabTitleDiv)
+    toDoMainContent.appendChild(taksAndToDoDIv)
+
+
+    pageContent.appendChild(headerDiv)
+    pageContent.appendChild(sideBarDiv)
+    pageContent.appendChild(toDoMainContent)
+    return pageContent
+}
+
+
+export{
+    loadPage,
+    createAnElement,
+    createInput,
+    createLabel
+}
